4e3707cd017c583c0216eb17dc8c2ba7eee6e608
Philipp Gabler <phipsgabler@users.noreply.github.com>
2019-11-16 23:33:58 +0100

    Try to implement model

--- lda.jl
+++ lda.jl
@@ -1,6 +1,7 @@
-using Turing
 using SparseArrays
 using DataStructures: Accumulator, counter
+using Turing
+using Distributions
 
 
 function extract_TDM(file = "epigrams.txt")
@@ -24,3 +25,39 @@ function extract_TDM(file = "epigrams.txt")
 end
 
 
+@model LatentDirichletAllocation(W, ntopics = 10, α = 0.5, β = 0.5) = begin
+    ndocs, ntypes = size(W)
+    ntokens = sum(W)
+
+    # prior for topic distribution per document
+    θ = Vector{Vector{Real}}(undef, ndocs)
+    for d = 1:ndocs
+        θ[d] ~ Dirichlet(ntopics, α)
+    end
+
+    # topic distribution per document
+    Z = Vector{Vector{Int}}(undef, ndocs)
+    for d = 1:ndocs
+        z[d] ~ Categorical(θ[d])
+    end
+
+    # prior for topic distribution per word
+    ϕ = Vector{Vector{Real}}(undef, ntopics)
+    for t = 1:ntopics
+        ϕ[t] ~ Dirichlet(ntypes, β)
+    end
+
+    # distribution of word per document
+    for d = 1:ndocs, w = 1:ntypes
+        W[d, w] ~ Categorical(ϕ[Z[d][w]])
+    end
+end
+
+
+function test()
+    W = extract_TDM()
+    chain = sample(LatentDirichletAllocation(W), IS(), 10)
+    describe(chain)
+end
+
+test()
