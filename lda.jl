791a73e2db878fc4a7bf74c44eb519011a937808
Philipp Gabler <phipsgabler@users.noreply.github.com>
2019-11-16 22:21:21 +0100

    Fix extract_TDM to use counter

--- lda.jl
+++ lda.jl
@@ -1,21 +1,22 @@
 using Turing
 using SparseArrays
+using DataStructures: Accumulator, counter
 
 
 function extract_TDM(file = "epigrams.txt")
-    epigram_bags = Vector{Set{UInt64}}()
+    epigram_bags = Vector{Accumulator{UInt64, Int8}}()
 
     for epigram in eachline(file)
-        push!(epigram_bags, Set(hash.(split(epigram))))
+        push!(epigram_bags, counter(hash.(split(epigram))))
     end
 
     D = length(epigram_bags)
     T = length(reduce(union, epigram_bags))
-    W = sparse([], [], Int[], D, T)
+    W = sparse([], [], Int8[], D, T)
 
     for (epigram, bag) in enumerate(epigram_bags)
-        for word in bag
-            W[document, Int(rem(word, T)) + 1] += 1
+        for (word, count) in bag
+            W[epigram, Int(rem(word, T)) + 1] += count
         end
     end
 
